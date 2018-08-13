996e8b11094e84bafeb7b9cfbe02f7374f6de028
Tim Weri <ng.duc.tahn@gmail.com>
2018-08-13 23:34:10 +0700

    Fix wrong callback function's arguments

--- app/src/main/java/project/timweri/androidcamerafilter/FilterSelection.java
+++ app/src/main/java/project/timweri/androidcamerafilter/FilterSelection.java
@@ -3,6 +3,7 @@ package project.timweri.androidcamerafilter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FilterSelection extends AppCompatActivity {
 
@@ -17,7 +18,7 @@ public class FilterSelection extends AppCompatActivity {
         setContentView(R.layout.activity_filter_selection);
     }
 
-    protected void showSolidBlend() {
+    protected void showSolidBlend(View view) {
         Intent intent = new Intent(this, CameraTest.class);
         startActivity(intent);
     }
