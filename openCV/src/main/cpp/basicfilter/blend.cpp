7ef11c9be40b63e7b4f534b2b5677172592bc0e5
timweri <ng.duc.tahn@gmail.com>
2018-08-13 20:47:06 +0700

    Simple solid color blending

---
+++ openCV/src/main/cpp/basicfilter/blend.cpp
@@ -0,0 +1,22 @@
+#include <jni.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+extern "C"
+void JNICALL Java_project_timweri_basicfilter_BasicFilter_solidBlendRGBA(JNIEnv *env, jobject instance,
+                                                                         jlong matAddrRBGA,
+                                                                         jbyte R,
+                                                                         jbyte G,
+                                                                         jbyte B,
+                                                                         jfloat weight,
+                                                                         jboolean reset_cache
+) {
+    Mat *inputFrame = (Mat *) matAddrRBGA;
+    Scalar color = Scalar(R, G, B);
+    static Mat solid_color;
+    if (reset_cache) solid_color = Mat(inputFrame->rows, inputFrame->cols, CV_8UC3, color);
+    addWeighted(*inputFrame, 1 - weight, solid_color, weight, 0.0, *inputFrame);
+}
