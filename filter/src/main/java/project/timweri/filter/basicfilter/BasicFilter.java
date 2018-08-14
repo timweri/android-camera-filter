64ba77d1722a8c54d562c88ba159eb868b23dec2
timweri <ng.duc.tahn@gmail.com>
2018-08-14 17:36:21 +0700

    [Incomplete] Add Linear Interpolation

--- filter/src/main/java/project/timweri/filter/basicfilter/BasicFilter.java
+++ filter/src/main/java/project/timweri/filter/basicfilter/BasicFilter.java
@@ -7,6 +7,9 @@ import org.opencv.core.Size;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import project.timweri.filter.Filter;
 
 public class BasicFilter extends Filter {
@@ -48,7 +51,7 @@ public class BasicFilter extends Filter {
         Imgproc.GaussianBlur(inputFrame, inputFrame, new Size(0, 0), sigmaX, sigmaY);
     }
 
-    public native void gaussianblurRGBA(long matAddrRGBA, double sigmaX, double sigmaY, int borderType, boolean reset_cache);
+    // public native void gaussianblurRGBA(long matAddrRGBA, double sigmaX, double sigmaY, int borderType, boolean reset_cache);
 
     public class GaussianBlur implements BasicFilter.Blend {
         public double sX, sY;
@@ -62,6 +65,32 @@ public class BasicFilter extends Filter {
         public void applyBlend(Mat inputFrame, boolean reset_cache) {
             gaussianblurRGBA(inputFrame, sX, sY, reset_cache);
             // gaussianblurRGBA(inputFrame.getNativeObjAddr(), sX, sY, reset_cache);
+        }
+    }
+
+    public native void linearInterpolate(long channelAddr, float[] values);
+
+    public class LinearInterpolate implements BasicFilter.Blend {
+        public float[] vals_R = {0, 1};
+        public float[] vals_G = {0, 1};
+        public float[] vals_B = {0, 1};
+
+        public LinearInterpolate(float[] values_R, float[] values_G, float[] values_B) {
+            if (values_R != null) vals_R = values_R;
+            if (values_G != null) vals_G = values_G;
+            if (values_B != null) vals_B = values_B;
+        }
+
+        @Override
+        public void applyBlend(Mat inputFrame, boolean reset_cache) {
+            List<Mat> channels = new ArrayList(3);
+            Core.split(inputFrame, channels);
+
+            //linearInterpolate(channels.get(0).getNativeObjAddr(), vals_R);
+            linearInterpolate(channels.get(1).getNativeObjAddr(), vals_G);
+            linearInterpolate(channels.get(2).getNativeObjAddr(), vals_B);
+
+            Core.merge(channels, inputFrame);
         }
     }
 }
