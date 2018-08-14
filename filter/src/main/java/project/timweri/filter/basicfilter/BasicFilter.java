c0b5b777816917c478b666f20a3c8153854cadfe
timweri <ng.duc.tahn@gmail.com>
2018-08-14 16:08:26 +0700

    Reorganize SolidBlend and Implement Gaussian Blur

--- filter/src/main/java/project/timweri/filter/basicfilter/BasicFilter.java
+++ filter/src/main/java/project/timweri/filter/basicfilter/BasicFilter.java
@@ -1,6 +1,11 @@
 package project.timweri.filter.basicfilter;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.core.Size;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import project.timweri.filter.Filter;
 
@@ -8,6 +13,17 @@ public class BasicFilter extends Filter {
     static {
         System.loadLibrary("filter");
     }
+
+    private Mat solid_color;
+
+    public void solidBlendRGBA(Mat inputFrame, char R, char G, char B, float weight, boolean reset_cache) {
+        if (reset_cache || solid_color.empty()) {
+            solid_color = inputFrame.clone();
+            solid_color = solid_color.setTo(new Scalar(R, G, B));
+        }
+        Core.addWeighted(inputFrame, 1 - weight, solid_color, weight, 0, inputFrame);
+    }
+    public native void solidBlendRGBA(long matAddrRGBA, char R, char G, char B, float weight, boolean reset_cache);
 
     public class SolidBlend implements BasicFilter.Blend {
         public char R, G, B;
@@ -22,9 +38,30 @@ public class BasicFilter extends Filter {
 
         @Override
         public void applyBlend(Mat inputFrame, boolean reset_cache) {
-            solidBlendRGBA(inputFrame.getNativeObjAddr(), R, G, B, w, reset_cache);
+            solidBlendRGBA(inputFrame, R, G, B, w, reset_cache);
+            // solidBlendRGBA(inputFrame.getNativeObjAddr(), R, G, B, reset_cache);
         }
     }
 
-    public native void solidBlendRGBA(long matAddrRGBA, char R, char G, char B, float weight, boolean reset_cache);
+    // TODO: This is very slow. Implement ThreadPool to check if FPS improves.
+    public void gaussianblurRGBA(Mat inputFrame, double sigmaX, double sigmaY, boolean reset_cache) {
+        Imgproc.GaussianBlur(inputFrame, inputFrame, new Size(0, 0), sigmaX, sigmaY);
+    }
+
+    public native void gaussianblurRGBA(long matAddrRGBA, double sigmaX, double sigmaY, int borderType, boolean reset_cache);
+
+    public class GaussianBlur implements BasicFilter.Blend {
+        public double sX, sY;
+
+        public GaussianBlur(double sigmaX, double sigmaY) {
+            sX = sigmaX;
+            sY = sigmaY;
+        }
+
+        @Override
+        public void applyBlend(Mat inputFrame, boolean reset_cache) {
+            gaussianblurRGBA(inputFrame, sX, sY, reset_cache);
+            // gaussianblurRGBA(inputFrame.getNativeObjAddr(), sX, sY, reset_cache);
+        }
+    }
 }
